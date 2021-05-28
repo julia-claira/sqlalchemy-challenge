b6cf47d66c3ecc9721186e357c9831ea1a880518
Claira J <julia.claira@gmail.com>
2021-05-28 11:16:22 -0700

    fixed jupyter notebook and continued on flask

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine,func
+import datetime as dt
 
 from flask import Flask, jsonify
 
@@ -22,7 +23,12 @@ app=Flask(__name__)
 #Use Flask to define my routes
 @app.route("/")
 def home():
-    return "/api/v1.0/precipitation"
+    return (
+        f"Welcome To Julia's SQLAlchemy Weather API<br>"
+        f"Available Routes:<br>"
+        f"/api/v1.0/precipitation<br>"
+        f"/api/v1.0/stations<br>"
+        f"/api/v1.0/tobs<br>")
 
 @app.route("/api/v1.0/precipitation")
 def prec():
@@ -38,15 +44,42 @@ def prec():
     
     return jsonify(prec_dict)
     
-#@app.route("/api/v1.0/stations")
-#def home():
- #   print("api instructions")
+@app.route("/api/v1.0/stations")
+def stations():
+    session=Session(engine)
+    station_query=session.query(Station.station).distinct().all()
+    session.close()
+ 
+    # return list
+    station_list=[]
     
+    for station in station_query:
+        station_list.append(station[0])
+        
+    return jsonify(station_list)
+
+       
+@app.route("/api/v1.0/tobs")
+def Tobs():
     
-#@app.route("/api/v1.0/tobs")
-#def home():
- #   print("api instructions")
+    #sets up the year range that was found in my jupyter notebook query
+    oneYearAgoDate=dt.date(2017,8,23)-dt.timedelta(days=365)
+    mostRecentDate=dt.date(2017,8,23)
     
+    #finds most active station
+    session=Session(engine)
+    mostactive=session.query(Measurement.station)\
+    .group_by('station').order_by(func.count(Measurement.id).desc()).all()[0][0]                            
+    #filters for most active between the year set
+    mostactiveYearlyTemp=session.query(Measurement.date,Measurement.tobs)\
+    .filter(Measurement.station==mostactive)\
+    .filter(Measurement.date>=oneYearAgoDate)\
+    .filter(Measurement.date<=mostRecentDate).all()
+
+    session.close()
+    
+
+    return jsonify(mostactiveYearlyTemp)
     
 #@app.route("/api/v1.0/<start> and /api/v1.0/<start>/<end>")
 #def home():
